a3df5eb5ae04d267c4fe1725d596d6d1b7c4dc91
atharellahi <official.atharellahi@gmail.com>
2022-10-07 02:38:48 +0530

    update

--- src/Componets/Serives.js
+++ src/Componets/Serives.js
@@ -4,15 +4,16 @@ const Services = () => {
     return (<React.Fragment>
 
         <div id="services">
-                <div className="projectitem">
+                <div className="serviceitem">
                     <a href='https://www.instagram.com/koungbuilds' target='_blank' rel="noreferrer">
-                        <div className="imgproj">
-                            <img src='https://www.incimages.com/uploaded_files/image/1920x1080/getty_815430462_412039.jpg' alt="3dprinting"></img>
+                    <div className="srvctitle">
+                            3D PRINTING
                         </div>
-                        <div className="title">
-                            3d prining
+                        <div className="srvcimgproj">
+                            
                         </div>
-                        <div className="description">Order custom 3d printed products</div></a>
+            
+                        <div className="srvcdescription">Order custom 3d printed products</div></a>
                 </div>
         </div>
     </React.Fragment>
