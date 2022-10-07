f89f3a79f220e404ca1d033072c7c0d1098030f7
atharellahi <official.atharellahi@gmail.com>
2022-10-07 12:07:36 +0530

    updated

--- src/Componets/About.js
+++ src/Componets/About.js
@@ -1,9 +1,20 @@
 import React from "react";
+import avatar from './Images/avatar.jpg'
 
 const About = () => {
   return (<React.Fragment>
     <div id="about">
-    About
+      <div id="ava">
+        <img id='avatar' src={avatar}></img>
+      </div>
+      <div id="detail">
+
+        A Creator when it comes to mind<br></br>
+        A Gamer at heart<br></br>
+        Born a Maker<br></br>
+
+        I'm Athar Ellahi , thanks for visiting my website
+      </div>
     </div>
   </React.Fragment>
   )
